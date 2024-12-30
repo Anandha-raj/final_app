634ac263f40bf48c4ae5ebf6d8f8f84c0212f5e4
Anandharaj <anandharaj111097@gmail.com>
2024-12-30 23:21:56 +0530

    Made minor change

--- frontend/src/apicall/api.js
+++ frontend/src/apicall/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // we have to set the base url for the backend
 const api = axios.create({
-    baseURL: "https://fullstack-app-mxiu.onrender.com/api/",
+    baseURL: "http://localhost:5004/api/",
 })
 
 export default api;
